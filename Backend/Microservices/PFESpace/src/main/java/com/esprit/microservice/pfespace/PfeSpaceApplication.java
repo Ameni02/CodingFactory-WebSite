8cea142a055f1788659d7e99b2f6a691c78d711d
Ameni02 <ameni.zoubeir@esprit.tn>
2025-02-11 21:04:02 +0100

    architecture done

---
+++ Backend/Microservices/PFESpace/src/main/java/com/esprit/microservice/pfespace/PfeSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.esprit.microservice.pfespace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfeSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfeSpaceApplication.class, args);
+	}
+
+}
